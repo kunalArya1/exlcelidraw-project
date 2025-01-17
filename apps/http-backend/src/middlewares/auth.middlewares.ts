e4e77ae904858939a057b9b7d7ecaa9154f968bf
kunalArya1 <kunalkrraj220@gmail.com>
2025-01-17 09:59:27 +0530

    Middlewares Added

--- apps/http-backend/src/middlewares/auth.middlewares.ts
+++ apps/http-backend/src/middlewares/auth.middlewares.ts
@@ -1,7 +1,9 @@
-import { NextFunction } from "express";
+import { Request, Response, NextFunction } from "express";
 
-const { Request, Response, NextFunction } = require("@types/express");
-
-export const isAuth = async (req: Request, res: Response, next: NextFunction) => {
+export const isAuth = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const token: any = req.headers;
 };
